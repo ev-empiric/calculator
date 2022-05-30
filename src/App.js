b48d17681b19e8f599e0d15ee7a1a3f5ea081880
ev-empiric <ev.empiric@gmail.com>
2022-05-30 18:08:43 +0530

    complete calculator

--- src/App.js
+++ src/App.js
@@ -7,10 +7,16 @@ function App() {
   const [calc, setcalc] = useState("");
   const [result, setresult] = useState("");
 
-  const ops = ['/', '*', '+', '-', '.'];
+  const ops = ['/', '*', '+', '-', '.', '%'];
 
   const updateCalc = value => {
-    if (ops.includes(value) && calc === '' || ops.includes(value) && ops.includes(calc.slice(-1))) {
+
+    var mainMath = "0";
+    var subMath = "00";
+
+    if(value === mainMath || value === subMath){
+      return;
+    }else if (ops.includes(value) && calc === '' || ops.includes(value) && ops.includes(calc.slice(-1))) {
       return;
     }
     setcalc(calc + value);
@@ -19,13 +25,22 @@ function App() {
     }
   }
 
-  const onclearall = () =>{
-    const value='';
+  const onclearall = () => {
+    const value = '';
     setresult(value);
     setcalc(value);
   }
 
-  const calculate=()=>{
+  const deleteLast = () => {
+    if (calc == '') {
+      return;
+    }
+    const value = calc.slice(0,-1);
+    setcalc(value);
+    setresult(value);
+  }
+
+  const calculate = () => {
     setcalc(result);
   }
 
@@ -34,41 +49,47 @@ function App() {
 
     for (let i = 1; i < 10; i++) {
       digits.push(
-        <button key={i} className="btn btn-light waves-effect" onClick={() => updateCalc(i.toString())}>{i}</button>
+        <button key={i} className="digit" onClick={() => updateCalc(i.toString())}>{i}</button>
       )
     }
 
     return digits;
   }
 
-
   return (
-    <div className="App">
-      <div className="calculator card">
-        <div className="calculator-screen z-depth-1">
-        {/* <input type="text" className="calculator-screen z-depth-1" value={result ? result : calc || "0"} disabled /> */}
-          {/* <span>0</span> */}
-          {result ? <small>({result})</small> : <small>{`0 `}</small>}
-          {calc || "0"}
+    <div className="calculator">
+      <div className="display">
+        {result ? <small>({calc})</small> : <small>{`0 `}</small>}
+        {result || "0"}
+      </div>
+
+
+      <div className="">
+        <div className="topRow">
+          <button className="operator" onClick={onclearall}>AC</button>
+          <button className="operator" onClick={onclearall}>CA</button>
+          <button className="operator" onClick={deleteLast}>DEL</button>
+          <button id="division" className="operator" onClick={() => updateCalc('/')}>/</button>
         </div>
 
+        <div className="inputs">
+        <div className="digits">
+        {createDigits()}
+        <button id="multiplication" className="digit" onClick={() => updateCalc('00')}>00</button>
+        <button className="digit" onClick={() => updateCalc('0')}>0</button>
+        <button id="multiplication" className="digit" onClick={() => updateCalc('.')}>.</button>
+          </div>
 
-        <div className="calculator-keys">
+          <div className="operators">
 
-          <button type="button" className="operator btn btn-info" onClick={() => updateCalc('+')}>+</button>
-          <button type="button" className="operator btn btn-info" onClick={() => updateCalc('-')}>-</button>
-          <button type="button" className="operator btn btn-info" onClick={() => updateCalc('*')}>&times;</button>
-          <button type="button" className="operator btn btn-info" onClick={() => updateCalc('/')}>&divide;</button>
-
-          {createDigits()}
-          <button type="button" className="btn btn-light waves-effect" onClick={() => updateCalc('0')}>0</button>
-          <button type="button" className="decimal function btn btn-secondary" onClick={() => updateCalc('.')}>.</button>
-          <button type="button" className="all-clear function btn btn-danger btn-sm" onClick={onclearall}>AC</button>
-
-          <button type="button" className="equal-sign operator btn btn-default" onClick={calculate}>=</button>
-
+            <button id="multiplication" className="operator" onClick={() => updateCalc('*')}>*</button>
+            <button id="subtraction" className="operator" onClick={() => updateCalc('-')}>-</button>
+            <button id="addition" className="operator" onClick={() => updateCalc('+')}>+</button>
+            <button id="equals" className="operator" onClick={calculate}>=</button>
+          </div>
         </div>
       </div>
+
     </div>
   );
 }
